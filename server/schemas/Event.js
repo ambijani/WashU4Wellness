a6800ec079541be2f9cc3da556ff4ab8698f185e
Alymuhammad Bijani <a.bijani@wustl.edu>
2024-10-19 21:32:45 -0500

    adding autoincrement for eventid

--- server/schemas/Event.js
+++ server/schemas/Event.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
+// Avoid overwriting the Counter model if it has already been registered
+const Counter = mongoose.models.Counter || mongoose.model('Counter', new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 }
+}));
+
 const eventSchema = new mongoose.Schema({
-  eventId: { type: String, required: true, unique: true },
+  eventId: { type: String, unique: true },
   username: { type: String, required: true },
   eventName: { type: String, required: true },
   activityType: { type: String, required: true },
@@ -9,5 +15,22 @@ const eventSchema = new mongoose.Schema({
   dateTimeLogged: { type: Date, default: Date.now }
 }, { timestamps: true });
 
-const Event = mongoose.model('Event', eventSchema);
+eventSchema.pre('save', function(next) {
+  const doc = this;
+  Counter.findByIdAndUpdate(
+    { _id: 'eventId' },
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true }
+  )
+  .then(function(counter) {
+    doc.eventId = counter.seq;
+    next();
+  })
+  .catch(function(error) {
+    return next(error);
+  });
+});
+
+// Create the Event model
+const Event = mongoose.models.Event || mongoose.model('Event', eventSchema);
 module.exports = Event;
